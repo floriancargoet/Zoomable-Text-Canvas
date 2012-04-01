a496cca49f42216f34e8d42ff1d2cae3ae5ecb82
Florian Cargoët <florian.cargoet+github@centrale-marseille.fr>
2012-04-01 18:25:45 +0200

    basic text positioning and fixed point zoom

---
+++ infinite.js
@@ -0,0 +1,76 @@
+var data = [{
+    x : 0,
+    y : 0,
+    z : 1,
+    t : 'Hello'
+},{
+    x : 70,
+    y : 15,
+    z : 16,
+    t : 'fantastic'
+},{
+    x : 71,
+    y : 16,
+    z : 32,
+    t : 'world!'
+}];
+
+window.onload = function(){
+    var c = document.getElementById('c');
+    var ctx = c.getContext('2d');
+    
+    ctx.font = '32px sans-serif';
+    ctx.textBaseline = 'top';
+    
+    var viewport = { // initial viewport
+        x : 0,
+        y : 0,
+        w : 800,
+        h : 600
+    };
+    
+    $(c).bind('mousewheel', function(event, delta, deltaX, deltaY) {
+        // fixed point zoom
+        var zoom = Math.pow(1.2, -delta); // delta always 1/-1?
+        var x = event.clientX - c.offsetLeft, y = event.clientY - c.offsetTop;
+        fpZoom(viewport, zoom, x, y);
+        draw(ctx, viewport);
+    });
+    
+    draw(ctx, viewport);
+};
+
+    
+function draw(ctx, viewport){
+    ctx.clearRect(0, 0, 800, 600);
+    ctx.save();
+    var s = 600/viewport.h;
+    ctx.scale(s, s);
+    ctx.translate(-viewport.x, -viewport.y);
+    
+    // debug 
+    ctx.strokeRect(0, 0, 800, 600);
+    
+    data.forEach(function(o){
+        ctx.save();
+        var zoom = o.z;
+        ctx.translate(o.x, o.y)
+        ctx.scale(1/zoom, 1/zoom);
+        ctx.fillText(o.t, 0, 0);
+        ctx.restore();
+    });
+    ctx.restore();
+}    
+
+function fpZoom(viewport, zoom, x, y){
+    // convert fixed point in absolute coordinates
+    var zoom_abs = viewport.h/600;
+    var x_abs = x * zoom_abs + viewport.x;
+    var y_abs = y * zoom_abs + viewport.y;
+    // update viewport
+    viewport.w *= zoom;
+    viewport.h *= zoom;
+    zoom_abs = viewport.h/600;
+    viewport.x = x_abs - x * zoom_abs;
+    viewport.y = y_abs - y * zoom_abs;
+}
