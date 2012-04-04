4fe61df408d8a1ef667ca2650a3cf3297ffb4d84
Florian Cargoët <florian.cargoet+github@centrale-marseille.fr>
2012-04-04 20:58:44 +0200

    now you can type text!

--- zoomable-canvas.js
+++ zoomable-canvas.js
@@ -29,6 +29,10 @@ ZTC.data = [{
     t : 'world!'
 }];
 
+ZTC.currentString = {
+    text : ''
+};
+
 ZTC.init = function(){
     var c = document.getElementById('c');
     var ctx = this.context = c.getContext('2d');
@@ -47,7 +51,8 @@ ZTC.init = function(){
 
     // mouse events
     var dd = false, x0, y0; // drag and drop
-    $(c).bind('mousewheel', function(event, delta, deltaX, deltaY) {
+    $(c).attr('tabindex', '0')
+    .bind('mousewheel', function(event, delta, deltaX, deltaY) {
         // fixed point zoom
         var zoom = Math.pow(1.1, -delta); // delta always 1/-1?
         var x = event.clientX - c.offsetLeft, y = event.clientY - c.offsetTop;
@@ -63,12 +68,37 @@ ZTC.init = function(){
         vp.dx = 0;
         vp.dy = 0;
     }).bind('mousemove', function(event){
+        ZTC.mouseX = event.clientX - c.offsetLeft;
+        ZTC.mouseY = event.clientY - c.offsetTop;
+
         if(dd){
             var zoom_abs = vp.h/600,
                 dx = (x0 - event.clientX) * zoom_abs,
                 dy = (y0 - event.clientY) * zoom_abs;
             vp.dx = dx;
             vp.dy = dy;
+        }
+    }).bind('keypress', function(event){
+        event.preventDefault();
+        if(event.keyCode !== 13){ // else -> return key
+            var c = String.fromCharCode(event.charCode);
+            ZTC.currentString = {
+                text : ZTC.currentString.text + c
+            };
+        } else {
+            if(ZTC.currentString.text){
+                var zoom = vp.h/600;
+                ZTC.data.push({
+                    t : ZTC.currentString.text,
+                    x : vp.x + ZTC.mouseX * zoom,
+                    y : vp.y + ZTC.mouseY * zoom,
+                    z : 1/zoom
+                });
+            }
+            // reset
+            ZTC.currentString = {
+                text : ''
+            };
         }
     });
 };
@@ -84,23 +114,26 @@ ZTC.draw = function(){
     ctx.scale(s, s);
     ctx.translate(-vp.x - vp.dx, -vp.y - vp.dy);
 
-    // debug
-    ctx.strokeRect(0, 0, 800, 600);
-
     ZTC.data.forEach(function(o){
-        var zoom = o.z, zh = zoom*vp.h;
+        var zh = o.z*vp.h;
 
         if(30 < zh && zh < 6000){
             ctx.save();
             ctx.translate(o.x, o.y);
-            ctx.scale(1/zoom, 1/zoom);
+            ctx.scale(1/o.z, 1/o.z);
             ctx.globalAlpha = Math.max(0, Math.min( zh/30 - 1 , 1));
             ctx.fillText(o.t, 0, 0);
             ctx.restore();
         }
     });
+
     ctx.restore();
 
+    // print the text being typed
+    if(ZTC.currentString.text){
+        ctx.globalAlpha = 1;
+        ctx.fillText(ZTC.currentString.text, ZTC.mouseX, ZTC.mouseY);
+    }
     // next frame
     requestAnimFrame(ZTC.draw);
 };
